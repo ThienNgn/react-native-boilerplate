94d2eb81554901829def82c4e3acac953d7ebeaa
Thien Nguyen <thienandcode@gmail.com>
2019-03-10 11:56:10 +0100

    Fix prettier rules and pretty command

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,25 @@
 module.exports = {
-    'extends': 'airbnb',
-    'parser': 'babel-eslint',
-    'env': {
-        'jest': true,
-    },
-    'rules': {
-        'no-use-before-define': 'off',
-        'react/jsx-filename-extension': 'off',
-        'react/prop-types': 'off',
-        'comma-dangle': 'off'
-    },
-    'globals': {
-        "fetch": false
-    }
+  parser: 'babel-eslint',
+  extends: 'airbnb',
+  plugins: ['react', 'flowtype', 'jsx-a11y', 'import'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/prefer-stateless-function': [2, { ignorePureComponents: true }],
+    'react/forbid-prop-types': [0, { forbid: [] }],
+    'import/extensions': [1, 'never', { svg: 'always' }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+    "no-underscore-dangle": ["error", { "allow": ["_embedded"] }],
+    "react/no-did-mount-set-state": 0,
+    "react/no-did-update-set-state": 0,
+  },
+  env: {
+    jest: true,
+  },
 };
